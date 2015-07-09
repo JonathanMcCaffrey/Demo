66d98f96053cd527cd09368ebc8bb8bd134ae30d
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-07-08 21:40:32 -0400

    Fixed enemy rotation

--- Demo/Assets/Scripts/Enemy.cs
+++ Demo/Assets/Scripts/Enemy.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-	
-	
-	float direction = 0; //360
-	
-	// Use this for initialization
+		
+
+	float speed = 4;
+
 	void Start () {
-		//direction = Random.Range (0, 360);
+		direction = Random.Range (0, 360);
+
+		speed = Random.Range (4, 12);
 	}
 	
-	float areaRadius = 6;
+	float areaRadius = 18;
+
+	float direction = 0;
+	
+	float totalDistance = 0;
 	
 	bool UpdateEnemyLimits () {
 		bool wasFlipped = false;
@@ -27,8 +32,6 @@ public class Enemy : MonoBehaviour {
 			
 			direction = Mathf.Atan2(gameObject.transform.position.z, gameObject.transform.position.x) * Mathf.Rad2Deg + 180;
 			
-			
-		//	Debug.Log(direction);
 			direction += Random.Range(-10,10);
 			
 			wasFlipped = true;
@@ -39,12 +42,10 @@ public class Enemy : MonoBehaviour {
 		
 		return wasFlipped;
 	}
-	
-	float speed = 12;
-	
+
 	void UpdateEnemyMovement () {
-		float x = Mathf.Cos (direction);
-		float z = Mathf.Sin (direction);
+		float x = Mathf.Cos (direction) * Time.deltaTime * speed;
+		float z = Mathf.Sin (direction) * Time.deltaTime * speed;
 		float fixedX = x + gameObject.transform.position.x;
 		float fixedZ = z + gameObject.transform.position.z;
 		gameObject.transform.position = new Vector3 (fixedX, gameObject.transform.position.y, fixedZ);
@@ -71,9 +72,30 @@ public class Enemy : MonoBehaviour {
 				wasRecentlyFlipped = false;
 			}
 		}
+
+		Refresh ();
+	}
+	
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+
+		float x = Mathf.Cos (direction) * 2;
+		float z = Mathf.Sin (direction) * 2;
+
+		Vector3 vec = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+		Gizmos.DrawLine(transform.position, vec);
+
+		Refresh ();
+	}
+
+	void Refresh() {
+		float x = Mathf.Cos (direction) * 2;
+		float z = Mathf.Sin (direction) * 2;
 		
+		Vector3 vec = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
 		
-		gameObject.transform.rotation = Quaternion.Euler (new Vector3(0, direction, 0));
-		
+		gameObject.transform.LookAt (vec);
+
 	}
 }

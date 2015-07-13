61407020c99ecd4854adf7ca7425d63cf3e993ed
Jonathan McCaffrey <jon.mcc_social@live.com>
2015-07-12 23:33:04 -0400

    Clean up.

--- Demo/Assets/Scripts/Bullet.cs
+++ Demo/Assets/Scripts/Bullet.cs
@@ -12,7 +12,6 @@ public class Bullet : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Enemy") {
-			//Destroy(other.gameObject);
 			Destroy(gameObject);
 
 			ScoreManager.KilledEnemy();
--- Demo/Assets/Scripts/Enemy.cs
+++ Demo/Assets/Scripts/Enemy.cs
@@ -2,42 +2,40 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-		
+	
 	private Angle angle;
 	private float speed = 4;
-
+	
 	void Start () {
 		angle = GetComponent<Angle> ();
 		speed = Random.Range (4, 12);
-
+		
 		EnemyManager.get().enemies.Add (this);
 	}
-
+	
 	void OnDestroy() {
 		EnemyManager.get ().enemies.Remove (this);
 	}
-
+	
 	float areaRadius = 22;
-	float totalDistance = 0;
-
+	
 	void OnTriggerEnter2D(Collider2D other) {
-
-
+		
+		
 		if (other.tag != "Wavepoint") {
 			return;
 		}
-
+		
 		WavePoint point = other.GetComponent<WavePoint> ();
-
+		
 		if (point.next == null) {
 			GameObject.Destroy(gameObject);
 			return;
 		}
 
-		//if (point.getAngle ().IsWithin (angle.getDegreeValue(), 90)) {
-
-			angle.setDegreeValue(point.getAngle().getDegreeValue());
-		//}
+		if (angle.IsWithin (point.getAngle ().getDegreeValue (), 90)) {
+			angle.setDegreeValue (point.getAngle ().getDegreeValue ());
+		}
 	}
 	
 	
@@ -54,19 +52,17 @@ public class Enemy : MonoBehaviour {
 			
 			Vector2 direction = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
 			direction = direction.normalized;
-
+			
 			angle.setDegreeValue(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180);
 			angle.setDegreeValue(angle.getDegreeValue() + Random.Range(-10,10));
 			
 			wasFlipped = true;
 		}
-
+		
 		return wasFlipped;
 	}
-
+	
 	void UpdateEnemyMovement () {
-
-
 		float y = angle.getDirection ().y * Time.deltaTime * speed;
 		float x = angle.getDirection ().x * Time.deltaTime * speed;
 		float fixedX = x + gameObject.transform.position.x;
@@ -81,7 +77,6 @@ public class Enemy : MonoBehaviour {
 	float flipTimer = 0;
 	float timerMax = 0.2f;
 	void Update () {
-		
 		previousPosition = gameObject.transform.position;
 		
 		UpdateEnemyMovement ();
@@ -95,7 +90,7 @@ public class Enemy : MonoBehaviour {
 				wasRecentlyFlipped = false;
 			}
 		}
-
+		
 		Refresh ();
 	}
 	
@@ -103,20 +98,20 @@ public class Enemy : MonoBehaviour {
 		if (angle == null) {
 			return;
 		}
-
-
+		
+		
 		Gizmos.color = Color.red;
-
+		
 		float y = angle.getDirection ().y;
 		float x = angle.getDirection ().x;
-
+		
 		Vector3 vec = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
-
+		
 		Gizmos.DrawLine(transform.position, vec);
-
+		
 		Refresh ();
 	}
-
+	
 	void Refresh() {
 		float y = angle.getDirection ().y;
 		float x = angle.getDirection ().x;
@@ -124,7 +119,7 @@ public class Enemy : MonoBehaviour {
 		Vector3 vec = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z);
 		
 		gameObject.transform.LookAt (vec);
-
-
+		
+		
 	}
 }
